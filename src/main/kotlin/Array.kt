3e564ca58fc29f7995d9ab2615d5844f576b8c03
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-06-23 20:06:15 +0900

    Add sorted insert

--- src/main/kotlin/Array.kt
+++ src/main/kotlin/Array.kt
@@ -4,9 +4,23 @@ fun main(args: Array<String>) {
     array[1] = 17
     array[2] = 80
     array[3] = 202
-    println(array.joinToString { "$it" })
-    insert(array, 5, 2)
-    println(array.joinToString { "$it" })
+//    println(array.joinToString { "$it" })
+//    insert(array, 5, 2)
+//    println(array.joinToString { "$it" })
+
+    val orderedArray = array.copyOf()
+    println(orderedArray.joinToString { "$it" })
+    sortedInsert(orderedArray, 19)
+    println(orderedArray.joinToString { "$it" })
+}
+
+fun sortedInsert(a: Array<Int?>, v: Int) {
+    for(i in 0..a.size) {
+        if (a[i]!! > v) {
+            insert(a, v, i)
+            return
+        }
+    }
 }
 
 fun insert(a: Array<Int?>, v: Int, index: Int) {
