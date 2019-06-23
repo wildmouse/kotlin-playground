8b9cf8cda87ef9a9cffeb302642cc057f63d0c0e
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-06-23 20:24:27 +0900

    Fix array insert

--- src/main/kotlin/Array.kt
+++ src/main/kotlin/Array.kt
@@ -1,22 +1,23 @@
 fun main(args: Array<String>) {
-    val array = arrayOfNulls<Int>(10)
-    array[0] = 3
-    array[1] = 17
-    array[2] = 80
-    array[3] = 202
-//    println(array.joinToString { "$it" })
-//    insert(array, 5, 2)
-//    println(array.joinToString { "$it" })
 
-    val orderedArray = array.copyOf()
+    val orderedArray = arrayOfNulls<Int>(10)
+    sortedInsert(orderedArray, 1)
+    sortedInsert(orderedArray, 2)
+    sortedInsert(orderedArray, 3)
+    sortedInsert(orderedArray, 4)
+    sortedInsert(orderedArray, 5)
+    sortedInsert(orderedArray, 6)
+    sortedInsert(orderedArray, 7)
+    sortedInsert(orderedArray, 8)
+    sortedInsert(orderedArray, 9)
     println(orderedArray.joinToString { "$it" })
     sortedInsert(orderedArray, 19)
     println(orderedArray.joinToString { "$it" })
-    searchOrdered(orderedArray, 19)
-    searchOrdered(orderedArray, 18)
+    linearSearchOrdered(orderedArray, 19)
+    linearSearchOrdered(orderedArray, 18)
 }
 
-fun searchOrdered(a: Array<Int?>, v: Int) {
+fun linearSearchOrdered(a: Array<Int?>, v: Int) {
     for(i in 0..a.size) {
         if (a[i] == null || a[i]!! > v) {
             println("Target value is not contained.")
@@ -31,7 +32,7 @@ fun searchOrdered(a: Array<Int?>, v: Int) {
 
 fun sortedInsert(a: Array<Int?>, v: Int) {
     for(i in 0..a.size) {
-        if (a[i]!! > v) {
+        if (a[i] == null || a[i]!! > v) {
             insert(a, v, i)
             return
         }
@@ -42,7 +43,7 @@ fun insert(a: Array<Int?>, v: Int, index: Int) {
     if (index > a.size) {
         throw Exception("Array out of bound.")
     }
-    for (i in (a.size-1) downTo index) {
+    for (i in (a.size-1) downTo index + 1) {
         a[i] = a[i-1]
     }
     a[index] = v
