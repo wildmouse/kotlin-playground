e5885ba79dbf2c9b478028a77866eeb90f6c379d
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-06-26 07:34:56 +0900

    Add hash map

---
+++ src/main/kotlin/HashTable.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val hashMap = hashMapOf<String, Double>(
+        "french fries" to 0.75,
+        "hamburger" to 2.5,
+        "hot dog" to 1.5,
+        "soda" to 0.6
+    )
+    println(hashMap["soda"])
+}
