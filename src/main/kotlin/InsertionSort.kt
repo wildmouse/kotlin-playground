95866f6445aabb4905e15297abffa85b197a505e
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-06-26 06:57:18 +0900

    Update insertion sort

--- src/main/kotlin/InsertionSort.kt
+++ src/main/kotlin/InsertionSort.kt
@@ -6,14 +6,14 @@ fun main() {
 }
 
 fun insertionSort(a: Array<Int>): Array<Int> {
-    for (i in 1..(a.size-1)) {
+    for (i in 1 until a.size) {
+        var index = i
         val tmpValue = a[i]
-        for (j in (i-1) downTo 0) {
-            if (a[j] > tmpValue) {
-                a[j+1] = a[j]
-                a[j] = tmpValue
-            }
+        while (index > 0 && a[index-1] > tmpValue) {
+                a[index] = a[index-1]
+                index--
         }
+        a[index] = tmpValue
         println(a.joinToString { it.toString() })
     }
     return a
