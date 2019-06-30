41bdd3529e4bb092085c1a755d5f7a06f2b5a478
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-07-01 06:48:33 +0900

    Add inserting node

--- src/main/kotlin/chapter12/BinaryTree.kt
+++ src/main/kotlin/chapter12/BinaryTree.kt
@@ -5,6 +5,7 @@ fun main() {
     val aNode2 = Node(10)
     val tree = BinaryTree(Node(5, aNode1, aNode2))
 
+    tree.insert(8)
     val target = tree.search(8)
     println(target?.value.toString())
 }
@@ -15,16 +16,55 @@ class BinaryTree(
     fun search(v: Int, node: Node? = root): Node? {
         return if (node == null || node.value == v) {
             node
-        } else if (v < root.value) {
+        } else if (v < node.value) {
             search(v, node.left)
         } else {
             search(v, node.right)
         }
     }
+
+    fun insert(v: Int, node: Node = root) {
+        val newNode = Node(v)
+        if (node.value == v) {
+            println("A value already exists.")
+            return
+        }
+        if (node.value > v && node.left == null) {
+            node.left = newNode
+            return
+        }
+        if (node.value > v) {
+            insert(v, node.left!!)
+        }
+        if (node.value < v && node.right == null) {
+            node.right = newNode
+            return
+        }
+        if (node.value < v) {
+            insert(v, node.right!!)
+        }
+    }
 }
 
 class Node(
-    val value: Int,
-    val left: Node? = null,
-    val right: Node? = null
+    var value: Int,
+    var left: Node? = null,
+    var right: Node? = null
 )
+
+//class Node(
+//    var value: Int,
+//    var left: Node? = null,
+//    var right: Node? = null
+//) {
+//    fun insert(v: Int) {
+//        val newNode = Node(v)
+//        if (value > v && left == null) {
+//            left = newNode
+//            return
+//        }
+//        if (value > v) {
+//            insert(v, left!!)
+//        }
+//    }
+//}
